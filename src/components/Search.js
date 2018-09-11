3d94682c93c39479f04a84c0010dbe2554e2d742
Susan Nio <susan.wen88@gmail.com>
2018-09-12 00:19:21 +0700

    Adding Search feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import axios from 'axios'
-import {StyleSheet, View, TextInput, TouchableOpacity, Text} from 'react-native';
-
-
+import {StyleSheet, View, TextInput, TouchableOpacity, FlatList} from 'react-native';
+import YTSearch from 'youtube-api-search';
+import VideoItem from './videoItem'
+const API_KEY = 'AIzaSyBugMM-xZoRiwlngP5Pm676T_PcAngpEZ8';
 export default class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            videos: []
+        };
+    }
+
     static navigationOptions = {
         header: null
+    }
+    videoSearch(term) {
+        YTSearch ({ key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                 videos: videos,
+             })
+             console.log(this.state.videos)
+        })
+       
+    }
+    handleKeyDown = (e) => {
+       if(e.nativeEvent.key==='Enter'){
+           console.log('Search this=>',this.state.term)
+           this.videoSearch(this.state.term)
+       }
     }
     render(){
         return(
@@ -15,15 +38,26 @@ export default class Search extends Component {
                     <TouchableOpacity>
                         <Icon onPress={() => this.props.navigation.navigate('Home')} name='arrow-back' size={25} />
                     </TouchableOpacity>
-                   
+                    <TextInput 
+                        style={styles.searchInput} 
+                        placeholder="Search YouTube" 
+                        onChangeText={(term) => this.setState({term})}
+                        onKeyPress={this.handleKeyDown.bind(this)}
+                        value={this.state.term}
+                        /> 
                     <View style={styles.rightNav}>
                         <TouchableOpacity>
                             <Icon style={styles.navItem} name='mic' size={25} />
                         </TouchableOpacity>
                     </View>
                 </View>
-                <View>
-                    <TextInput placeholder="Search YouTube"/> 
+                <View>  
+                    <FlatList
+                        data={this.state.videos}
+                        renderItem={(video) =><VideoItem navigation={this.props.navigation} video={video.item} />}
+                        keyExtractor={(item)=> item.etag}
+                        ItemSeparatorComponent={()=><View style={{height: 0.5, backgroundColor:"#cccccc"}}/>}
+                    />
                 </View>
             </View>
         )
@@ -32,7 +66,8 @@ export default class Search extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: 'white'
     },
     navBar: {
         height: 55,
@@ -49,6 +84,11 @@ const styles = StyleSheet.create({
     navItem: {
         marginLeft: 25,
     },
+    searchInput:{
+        width: 300,
+        paddingLeft: 30,
+        fontSize: 20,
+    }
     
 });
 
